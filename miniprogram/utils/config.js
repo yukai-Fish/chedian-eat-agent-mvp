8569b31ab9033ec537f9a7796a8d6dfd7ed1fd59
yukai-Fish <3136227858@qq.com>
2026-04-02 22:51:10 +0800

    chore(miniprogram): force Render API in dev mode

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,7 +1,11 @@
 const DEV_BASE_URL = "http://127.0.0.1:8000";
 const PROD_BASE_URL = "https://chedian-eat-agent-mvp.onrender.com";
+const FORCE_REMOTE_IN_DEV = true;
 
 function resolveApiBaseUrl() {
+  if (FORCE_REMOTE_IN_DEV) {
+    return PROD_BASE_URL;
+  }
   if (typeof __wxConfig !== "undefined" && __wxConfig.envVersion === "develop") {
     return DEV_BASE_URL;
   }
